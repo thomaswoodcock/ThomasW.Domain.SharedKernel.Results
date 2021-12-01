dc5625a94701a2b25cbaa8717c7aae4fb9640a48
Thomas Woodcock <thomas@thomaswoodcock.net>
2021-12-01 06:49:21 +0000

    Removes outer classes in tests

--- tests/ThomasW.Domain.SharedKernel.Results.UnitTests/PendingResultTests.cs
+++ tests/ThomasW.Domain.SharedKernel.Results.UnitTests/PendingResultTests.cs
@@ -1,50 +1,45 @@
 ﻿using FluentAssertions;
+
 using Xunit;
 
 namespace ThomasW.Domain.SharedKernel.Results.UnitTests;
 
-public sealed class PendingResultTests
+public class PendingResultTests
 {
-    public sealed class Success
+    [Fact]
+    public void Success_Value_ReturnsSuccessfulResult()
     {
-        [Fact]
-        public void Success_Value_ReturnsSuccessfulResult()
-        {
-            // Arrange
-            PendingResult<string> pendingResult = Result.Pending<string>();
+        // Arrange
+        PendingResult<string> pendingResult = Result.Pending<string>();
 
-            // Act
-            Result<string> result = pendingResult.Success("Test Value");
+        // Act
+        Result<string> result = pendingResult.Success("Test Value");
 
-            // Assert
-            result.IsSuccessful.Should().BeTrue();
-            result.Value.Should().Be("Test Value");
-            result.IsFailed.Should().BeFalse();
-            result.FailureReason.Should().BeNull();
-        }
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+        result.Value.Should().Be("Test Value");
+        result.IsFailed.Should().BeFalse();
+        result.FailureReason.Should().BeNull();
     }
 
-    public sealed class Fail
+    [Fact]
+    public void Fail_ValueType_ReturnsFailedResult()
     {
-        [Fact]
-        public void Fail_ValueType_ReturnsFailedResult()
-        {
-            // Arrange
-            PendingResult<string> pendingResult = Result.Pending<string>();
-            TestFailureReason failureReason = new();
+        // Arrange
+        PendingResult<string> pendingResult = Result.Pending<string>();
+        TestFailureReason failureReason = new();
 
-            // Act
-            Result<string> result = pendingResult.Fail(failureReason);
+        // Act
+        Result<string> result = pendingResult.Fail(failureReason);
 
-            // Assert
-            result.IsFailed.Should().BeTrue();
-            result.FailureReason.Should().Be(failureReason);
-            result.IsSuccessful.Should().BeFalse();
-            result.Value.Should().BeNull();
-        }
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        result.FailureReason.Should().Be(failureReason);
+        result.IsSuccessful.Should().BeFalse();
+        result.Value.Should().BeNull();
+    }
 
-        private sealed class TestFailureReason : FailureReason
-        {
-        }
+    private sealed class TestFailureReason : FailureReason
+    {
     }
 }
--- tests/ThomasW.Domain.SharedKernel.Results.UnitTests/ResultTests.cs
+++ tests/ThomasW.Domain.SharedKernel.Results.UnitTests/ResultTests.cs
@@ -1,86 +1,78 @@
 ﻿using FluentAssertions;
+
 using Xunit;
 
 namespace ThomasW.Domain.SharedKernel.Results.UnitTests;
 
 public sealed class ResultTests
 {
-    public sealed class Success
+    [Fact]
+    public void Success_NoValue_ReturnsSuccessfulResult()
     {
-        [Fact]
-        public void Success_NoValue_ReturnsSuccessfulResult()
-        {
-            // Arrange Act
-            Result result = Result.Success();
+        // Arrange Act
+        var result = Result.Success();
 
-            // Assert
-            result.IsSuccessful.Should().BeTrue();
-            result.IsFailed.Should().BeFalse();
-            result.FailureReason.Should().BeNull();
-        }
-
-        [Fact]
-        public void Success_Value_ReturnsSuccessfulResult()
-        {
-            // Arrange Act
-            Result<string> result = Result.Success("Test Value");
-
-            // Assert
-            result.IsSuccessful.Should().BeTrue();
-            result.Value.Should().Be("Test Value");
-            result.IsFailed.Should().BeFalse();
-            result.FailureReason.Should().BeNull();
-        }
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+        result.IsFailed.Should().BeFalse();
+        result.FailureReason.Should().BeNull();
     }
 
-    public sealed class Fail
+    [Fact]
+    public void Success_Value_ReturnsSuccessfulResult()
     {
-        [Fact]
-        public void Fail_NoValueType_ReturnsFailedResult()
-        {
-            // Arrange
-            TestFailureReason failureReason = new();
+        // Arrange Act
+        var result = Result.Success("Test Value");
 
-            // Act
-            Result result = Result.Fail(failureReason);
-
-            // Assert
-            result.IsFailed.Should().BeTrue();
-            result.FailureReason.Should().Be(failureReason);
-            result.IsSuccessful.Should().BeFalse();
-        }
-
-        [Fact]
-        public void Fail_ValueType_ReturnsFailedResult()
-        {
-            // Arrange
-            TestFailureReason failureReason = new();
-
-            // Act
-            Result<string> result = Result.Fail<string>(failureReason);
-
-            // Assert
-            result.IsFailed.Should().BeTrue();
-            result.FailureReason.Should().Be(failureReason);
-            result.IsSuccessful.Should().BeFalse();
-            result.Value.Should().BeNull();
-        }
-
-        private sealed class TestFailureReason : FailureReason
-        {
-        }
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+        result.Value.Should().Be("Test Value");
+        result.IsFailed.Should().BeFalse();
+        result.FailureReason.Should().BeNull();
     }
 
-    public sealed class Pending
+    [Fact]
+    public void Fail_NoValueType_ReturnsFailedResult()
     {
-        [Fact]
-        public void Pending_TypeValue_ReturnsPendingResult()
-        {
-            // Arrange Act
-            PendingResult<string> result = Result.Pending<string>();
+        // Arrange
+        TestFailureReason failureReason = new();
 
-            // Assert
-            result.Should().BeOfType<PendingResult<string>>();
-        }
+        // Act
+        var result = Result.Fail(failureReason);
+
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        result.FailureReason.Should().Be(failureReason);
+        result.IsSuccessful.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Fail_ValueType_ReturnsFailedResult()
+    {
+        // Arrange
+        TestFailureReason failureReason = new();
+
+        // Act
+        var result = Result.Fail<string>(failureReason);
+
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        result.FailureReason.Should().Be(failureReason);
+        result.IsSuccessful.Should().BeFalse();
+        result.Value.Should().BeNull();
+    }
+
+    [Fact]
+    public void Pending_TypeValue_ReturnsPendingResult()
+    {
+        // Arrange Act
+        PendingResult<string> result = Result.Pending<string>();
+
+        // Assert
+        result.Should().BeOfType<PendingResult<string>>();
+    }
+
+    private sealed class TestFailureReason : FailureReason
+    {
     }
 }
